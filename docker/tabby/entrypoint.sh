3bdec673757df8a34d8782e6fc1f2558cfe819e6
Matthew Rease <self@matthewrease.net>
2025-04-20 20:09:54 -0700

    Possibly finished Docker implementation (for w/tabby)

--- docker/tabby/entrypoint.sh
+++ docker/tabby/entrypoint.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/sh
 
+# Set model
+MODEL_NAME=$(basename "$MODEL")
+if [ "$MODEL_NAME" != "Reformed-Christian-Bible-Expert-v2.1-12B_EXL2_5.5bpw_H8" ]
+then
+	sed -i "s/^  model_name: .*/  model_name: $MODEL_NAME/" /app/tabbyAPI/config.yml
+fi
+
+if [ -d "/app/tabbyAPI/models/$MODEL_NAME" ]
+then
+	cd /app/tabbyAPI/models
+	git clone "$MODEL"
+fi
+
 cd /app/tabbyAPI
-python start.py
+python start.py &
+
+cd /app
+python discord_bot.py
